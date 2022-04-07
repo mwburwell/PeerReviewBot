6c1e1407cd33ab7073fa49ab329ded49846ad92b
mwburwell <mwburwell@gmail.com>
2022-04-07 17:55:52 -0400

    Working on some examples

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,32 +1,85 @@
-import os
 import discord
+from discord.ext import commands
+import random
 
 from Bot import GUILD_ID, STDOUT_ID
+
+
+# This example requires the 'members' and 'message_content' privileged intents to function.
+
 
 with open("./secrets/token") as f:
 	_token = f.read().strip()
 
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+
 intents = discord.Intents.default()
 intents.members = True
-client = discord.Client(intents=intents)
 
-@client.event
+
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
 async def on_ready():
-	# guild = client.get_guild(GUILD_ID)
-	print('We have logged in as {0.user}'.format(client))
-	# for member in guild.members:
-	# 	print(member)
-		
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
-		
-	if message.content.startswith('!hello'):
-		await message.channel.send('Change Permissions!')
-		return
-	
-	# if message.content.startswith('!createChannel'):
-	# 	channel = await discord.guild.TextChannel()
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-client.run(_token)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined in {member.joined_at}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+
+bot.run(_token)
