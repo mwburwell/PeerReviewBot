2d6a7d96831f64232e87b1ae53cbc67ee27f96ab
Map66 <72113548+Map66@users.noreply.github.com>
2022-04-26 22:01:40 -0400

    Add files via upload
    You'll have to dig through it to find what's new.

---
+++ Bot/Bot_Extra_pycord.py
@@ -0,0 +1,153 @@
+# Author Michael Peck
+
+from ast import Break
+from asyncore import read
+from http import client
+import imp
+from lib2to3.pgen2.token import SLASH
+from multiprocessing.connection import Client
+import os
+from pickle import FALSE, TRUE
+from pydoc import describe
+from tokenize import Token
+from unicodedata import name
+import discord
+from discord.ext import commands
+import discord.ui
+#from setuptools import Command
+from discord.commands import Option
+
+import Readme
+
+def main():
+
+    
+    print(discord.__version__)
+    
+    intent = discord.Intents.default()
+    intent.members = True
+    intent.message_content = True
+
+    client = commands.Bot(command_prefix="<>", intents = intent)
+
+    @client.event
+    async def on_ready():
+        print(f"{client.user.name} has connected")
+    
+    @client.slash_command(
+        name="new_teacher",
+        description="Promotes a use to Teacher",
+        guild_ids=[958468593358626816, 966429842532884490],
+    )
+    async def new_teacher(
+        ctx: discord.ApplicationContext,
+        user: Option(discord.Member, "Member to make a Teacher")
+    ):
+        if (ctx.author == ctx.guild.owner):
+            Teach = None
+            for r in ctx.guild.roles:
+                if (r.name == "Teacher"):
+                    Teach = r
+                    Break
+            await user.add_roles(Teach)
+            await ctx.respond("Complete!")
+
+    class Dropdown(discord.ui.Select):
+        def __init__(self, guild):
+            self.guild = guild
+            self.flag = True
+            # Set the options that will be presented inside the dropdown
+            options = [
+                discord.SelectOption(
+                    label="1", description="There is 1 Module"
+                ),
+                discord.SelectOption(
+                    label="4", description="There are 4 Modules"
+                ),
+                discord.SelectOption(
+                    label="8", description="There are 8 Modules"
+                ),
+                discord.SelectOption(
+                    label="16", description="There are 16 Modules"
+                ),
+            ]
+
+            super().__init__(
+                placeholder="How many modules are in the Class...",
+                min_values=1,
+                max_values=1,
+                options=options,
+            )  
+
+        async def callback(self, interaction: discord.MessageInteraction):
+                if self.flag:
+                    self.flag = False
+                    for i in range(1, int(self.values[0]) + 1):
+                        await self.guild.create_category(f"Module-{i}", position=i)
+                        for c in (self.guild.categories):
+                            if (c.name == f"Module-{i}"):
+                                Cat = c
+                        await self.guild.create_text_channel("Best-Of", category=Cat)
+                    await interaction.response.send_message(f"There are {self.values[0]} Modules.")
+                else:
+                    await interaction.response.send_message("Implementation already used.")
+
+    class DropdownView(discord.ui.View):
+        def __init__(self, guild):
+            self.guild = guild
+            super().__init__()
+            # Adds the dropdown to our view object.
+            self.add_item(Dropdown(self.guild))      
+
+    @client.event
+    async def on_guild_join(guild):
+        view = DropdownView(guild)
+        await guild.text_channels[0].send("How many Modules do you want?", view = view)
+        await guild.create_role(name="Teacher")
+        Teach = None
+        for r in guild.roles:
+            if (r.name == "Teacher"):
+                Teach = r
+                Break
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.me: discord.PermissionOverwrite(read_messages=True),
+            Teach: discord.PermissionOverwrite(read_messages=True)
+        }
+        await guild.create_category("Teachers_Channel", overwrites=overwrites, position=0)
+        for c in (guild.categories):
+            if (c.name == "Teachers_Channel"):
+                Cat = c
+                Break
+        await guild.create_text_channel("Moderate-Classroom", overwrites=overwrites, category=Cat)
+        await guild.create_text_channel("Bot-Info", overwrites=overwrites, category=Cat)
+        await Cat.text_channels[1].send(Readme.Readme.Readme())
+
+    @client.command()
+    async def ping(ctx):
+        """Checks for response form bot"""
+        guild = ctx.guild
+        view = DropdownView(guild)
+        await guild.text_channels[0].send("How many Modules do you want?", view = view)
+        await guild.create_role(name="Teacher")
+        Teach = None
+        for r in guild.roles:
+            if (r.name == "Teacher"):
+                Teach = r
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.me: discord.PermissionOverwrite(read_messages=True),
+            Teach: discord.PermissionOverwrite(read_messages=True)
+        }
+        await guild.create_category("Teachers_Channel", overwrites=overwrites, position=0)
+        for c in (guild.categories):
+            if (c.name == "Teachers_Channel"):
+                Cat = c
+        await guild.create_text_channel("Moderate-Classroom", overwrites=overwrites, category=Cat)
+        await guild.create_text_channel("Bot-Info", overwrites=overwrites, category=Cat)
+        await Cat.text_channels[1].send(Readme.Readme.Readme())
+
+    client.run("OTYyNzg3MTM2MjEyNDUxMzk4.YlMnZA.mG2oNY_zPTt9GVtT8AzRl8KIOnw")
+
+
+main()
